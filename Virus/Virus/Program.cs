c7929cd1f68005fbcdf90fc3200df3e784188171
Дмитрий Трефилов <trefilowdem@mail.ru>
2019-06-14 17:15:56 +0500

    Add function CreatePole

--- Virus/Virus/Program.cs
+++ Virus/Virus/Program.cs
@@ -9,9 +9,31 @@ namespace Virus
 {
     class Program
     {
+        private static int[,] Pole;
+        private static void CreatePole()
+        {
+            ColorMess.Yellow("\n Введите колличество строк в поле: ");
+            int n = Input.Check(1, 3000);
+            ColorMess.Yellow("\n Введите колличество столбцов в поле: ");
+            int m = Input.Check(1, 3000);
+            Pole = new int[n, m];
+            Console.Clear();
+            ColorMess.Yellow("\n Введите количество зараженных клеток: ");
+            int kol = Input.Check(1, 10);
+            for(int i = 0; i<kol; i++)
+            {
+                Console.Clear();
+                ColorMess.Yellow("\n Введите координату Х у " + (i + 1) + " точки: ");
+                int x = Input.Check(1, n);
+                ColorMess.Yellow("\n Введите координату Y у " + (i + 1) + " точки: ");
+                int y = Input.Check(1, m);
+                Pole[x,y] = 1;
+            }
+        }
         static void Main()
         {
-
+            CreatePole();
+            Message.GoToBack();
         }
     }
 }
